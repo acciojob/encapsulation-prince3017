ac99b1c3b4aa9164246db524c92a5056a73efa5d
prince3017 <princemahtojspr@gmail.com>
2023-01-13 16:22:19 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly sc = new RWOnly();
+        sc.setName("Prince");
+        System.out.println(sc.getName());
+    }
+
+
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String name = "";
+    public void setName(String s){
+        name = s;
+    }
+    public String getName(){
+        return name;
+    }
+
     
 }
